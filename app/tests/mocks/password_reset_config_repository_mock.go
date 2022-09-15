fc22c4db1f3703d10ce6f72705577c4e94552837
Mateus Silva <mateussilva@ntopus.com.br>
2022-09-15 13:23:51 -0300

    fix tests

---
+++ app/tests/mocks/password_reset_config_repository_mock.go
@@ -0,0 +1,23 @@
+package mocks
+
+import (
+	"github.com/mateus-sousa-dev/meet-people/app/domain"
+)
+
+type PasswordResetConfigRepositoryMock struct{}
+
+func NewPasswordResetConfigRepositoryMock() *PasswordResetConfigRepositoryMock {
+	return &PasswordResetConfigRepositoryMock{}
+}
+
+func (p *PasswordResetConfigRepositoryMock) FindPasswordResetConfigByUser(userID int64) *domain.PasswordResetConfig {
+	return nil
+}
+
+func (p *PasswordResetConfigRepositoryMock) CreatePasswordResetConfig(passwordResetConfig *domain.PasswordResetConfig) (*domain.PasswordResetConfig, error) {
+	return nil, nil
+}
+
+func (p *PasswordResetConfigRepositoryMock) UpdatePasswordResetConfig(passwordResetConfig *domain.PasswordResetConfig) (*domain.PasswordResetConfig, error) {
+	return nil, nil
+}
